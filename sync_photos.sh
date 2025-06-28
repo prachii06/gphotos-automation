f50eed9fb5dc80243790fdf7a3cf8ef7f1df1882
prachigohil-2006 <prachiigohil06@gmail.com>
2025-06-28 18:54:14 +0530

    sync_photos.sh

--- sync_photos.sh
+++ sync_photos.sh
@@ -2,6 +2,7 @@
 
 destination="/mnt/d/synced_photos" 
 credentials="client_secret.json" #google api credentials
+secret_path="/mnt/c/Users/Prachi Gohil/OneDrive/Desktop/gphotos-automation/client_secret.json"
 
 mkdir -p "$destination" #to create folder if not exists
                         #here , p ensures that there is no error if folder already exiists
@@ -11,7 +12,7 @@ echo "starting to sync"
 
 #sync command
 #to tell gphotos-sync tool which credentials file to use , --credentials is used
-gphotos-sync "$destination" --credentials "$credentials" >> log.txt 2>&1
+gphotos-sync "$destination" --secret "$secret_path" >> log.txt 2>&1
 # >>log.txt appends o/t & errors to seperate log file for proper tracking
 
 echo "synced"
